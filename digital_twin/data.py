512837bd28b55875929108d83eb47beab20f39b1
Sean McOwen <SeanMcOwen@gmail.com>
2022-02-09 11:57:29 -0500

    Add data pipeline funtions:

---
+++ digital_twin/data.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class DataPipeline(ABC):
+    @abstractmethod
+    def pull_historical_data(self):
+        pass
+    
+    @abstractmethod
+    def compute_input_data(self, data):
+        pass
