07ab143e74f6ca0feb4229224db5ce30a0fc7aa7
Sean McOwen <SeanMcOwen@gmail.com>
2022-06-24 09:45:01 -0400

    Add data processing

--- model2/data.py
+++ model2/data.py
@@ -67,3 +67,73 @@ def pull_trades(con: Connection) -> trade_table_raw:
 
     """
     return pd.read_sql("SELECT * FROM trades", con)
+
+
+def process_pure_returns(pure_returns_data: returns_table_raw) -> returns_table_processed:
+    """
+    Function to convert raw returns table to processed
+
+    Parameters
+    ----------
+    pure_returns_data : returns_table_raw
+        Raw version
+
+    Returns
+    -------
+    returns_table_processed
+        Processed version
+
+    """
+    
+    #Set and sort time index
+    pure_returns_data = pure_returns_data.set_index('t')
+    pure_returns_data = pure_returns_data.sort_index()
+    
+    return pure_returns_data
+
+def process_prices(prices_data: prices_table_raw) -> prices_table_processed:
+    """
+    Function to convert raw prices table to processed
+
+    Parameters
+    ----------
+    prices_data : prices_table_raw
+        Raw version
+
+    Returns
+    -------
+    prices_table_processed
+        Processed version
+
+    """
+    
+    #Set and sort index
+    prices_data = prices_data.set_index('t')
+    prices_data = prices_data.sort_index()
+    
+    return prices_data
+
+def process_trades(trades_data: trade_table_raw) -> trade_table_processed:
+    """
+    Function to convert raw trades table to processed
+
+    Parameters
+    ----------
+    trades_data : trade_table_raw
+        Raw version
+
+    Returns
+    -------
+    trade_table_processed
+        Processed version
+
+    """
+    
+    trades_data = trades_data.rename(columns = {"time": "t"})
+    
+    #Pivot to t x trade x had_trade
+    trades_data["had_trade"] = True
+    trades_data = trades_data.pivot("t", "trade", "had_trade")
+    trades_data = trades_data.fillna(False)
+    
+    return trades_data
