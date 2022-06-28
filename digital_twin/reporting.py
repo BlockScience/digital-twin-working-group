00c921b3303fabf23909116b0ce1d37247268591
Sean McOwen <SeanMcOwen@gmail.com>
2022-06-28 15:46:01 -0400

    Add in report creation pdf

--- digital_twin/reporting.py
+++ digital_twin/reporting.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+import os
+from pathlib import Path
+import papermill as pm
 
 class ReportingModule(ABC):
     
@@ -15,4 +18,19 @@ class ReportingModule(ABC):
         pass
     
     def create_backtest_report(self, backtest_template):
-        pass
+        runtime = "TEST"
+        working_path = Path(".")
+        path = "."
+        input_nb_path = (
+            working_path / backtest_template).expanduser()
+        output_nb_path = (
+            working_path / f'reports/{runtime}-Backtest.ipynb').expanduser()
+        output_html_path = (
+            working_path / f'reports/{runtime}-Backtest.html').expanduser()
+        pm.execute_notebook(
+            input_nb_path,
+            output_nb_path,
+            parameters=dict(base_path=path))
+        export_cmd = f"jupyter nbconvert --to html '{output_nb_path}'"
+        os.system(export_cmd)
+        os.system(f"rm '{output_nb_path}'")
